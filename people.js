29354b89bf46b8ce607611dc1c3774e907f62a2a
rodelaporte <rodelaporte@gmail.com>
2013-05-30 18:35:21 -0400

    Input interaction + array return

--- people.js
+++ people.js
@@ -1,7 +1,24 @@
+function add (item) {
+    var list = Session.get("list");
+    list.push(item);
+    Session.set("list", list);
+
+}
+
 if (Meteor.isClient) {
     Session.set("list", ["one", "two"]);
 
-    Template.list.items = function() {
+    Template.list.items = function () {
             return Session.get("list");
         };
+
+    Template.list.events({
+      'keypress input': function (e, t) {
+        if (e.keyCode === 13) {
+          var input = t.find("input");
+          add(input.value);
+          input.value = "";
+        }
+      }
+    });
 }
