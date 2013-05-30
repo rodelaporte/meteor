98ba10b5c88502d11548ae8d4f60f3a7e8f00059
rodelaporte <rodelaporte@gmail.com>
2013-05-30 19:51:06 -0400

    First Template

--- people.js
+++ people.js
@@ -1,24 +1,5 @@
-function add (item) {
-    var list = Session.get("list");
-    list.push(item);
-    Session.set("list", list);
-
-}
-
 if (Meteor.isClient) {
-    Session.set("list", ["one", "two"]);
-
-    Template.list.items = function () {
-            return Session.get("list");
-        };
-
-    Template.list.events({
-      'keypress input': function (e, t) {
-        if (e.keyCode === 13) {
-          var input = t.find("input");
-          add(input.value);
-          input.value = "";
-        }
-      }
-    });
+    Template.person.fullName = "Joe Smith";
+    Template.person.job = "Developer";
+    Template.person.likes = ["Bananas", "javascript", "Cars"]
 }
