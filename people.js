1f2f882ea8dfdea048fda8bb516e86e5d337a25d
rodelaporte <rodelaporte@gmail.com>
2013-05-30 14:56:53 -0400

    Second template with Strong CEO

--- people.js
+++ people.js
@@ -1,5 +1,12 @@
 if (Meteor.isClient) {
-  Template.person.firstname = "Joe Smith";
-  Template.person.name = "Developer";
-  Template.person.name = ["Bananas", "javascript", "Cars"];
+  Template.staff.people = [
+  	{ fullName: "John Doe", job: "CEO"},
+  	{ fullName: "Jane Smith", job: "CTO"},
+  	{ fullName: "Sophie Turner", job: "Developer"},
+  	{ fullName: "Jack Lewis", job: "Designer"},
+  	];
+
+  	Template.person.executive = function () {
+  		return !!this.job.match(/^C.*O$/);
+  	};
   };
